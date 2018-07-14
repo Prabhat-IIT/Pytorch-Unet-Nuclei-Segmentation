712ce32cef10ecaced47454058653299893a6a50
SHIVA-sopho <coolshiva.gandhi@gmail.com>
2018-07-14 19:45:41 +0530

    directly creates train and test stained images

---
+++ Data/createDataset.py
@@ -0,0 +1,31 @@
+from patches import createImgPatches, createBinMask
+import shutil
+import os
+from sklearn.model_selection import train_test_split
+
+IMG_DIR = './train-img256x256/'
+BIN_MASK_DIR = './train-bin-mask256x256/'
+
+source = '../segmentation_training_set/'
+patch_shape = 256
+
+if not os.path.isdir(IMG_DIR):
+	createBinMask(source)
+	createImgPatches(source, patch_shape)
+
+# img and mask files both have same names just different directories
+img_files = os.listdir('./train-img256x256')	
+
+train_files , test_files = train_test_split(img_files,test_size = 0.1)
+
+for file in train_files:
+	shutil.move(IMG_DIR + file, 'Train/Image/')
+	shutil.move(BIN_MASK_DIR + file, 'Train/Mask/')
+
+for file in test_files:
+	shutil.move(IMG_DIR + file, 'Test/Image/')
+	shutil.move(BIN_MASK_DIR + file , 'Test/Mask/')
+
+print('Done')
+		
+
