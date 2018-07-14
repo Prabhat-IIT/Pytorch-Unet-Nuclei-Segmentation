33843ad36d14c3d750a428d4752d05a88a96548d
SHIVA-sopho <coolshiva.gandhi@gmail.com>
2018-07-14 19:21:07 +0530

    added main patches.py

--- Data/patches.py
+++ Data/patches.py
@@ -3,9 +3,11 @@ import glob
 import numpy as np
 from skimage import io
 from patchHelper import extract_patches, pad_zeros
+from Utils import batchStainNormalize
 
 
-def createImgPatches(source,patch_size):
+
+def createImgPatches(source,patch_size,normalize = True):
     """Takes input source folder name and patch_size
         creates patches of given patch_size, creates a folder
         and saves patches in them
@@ -36,23 +38,26 @@ def createImgPatches(source,patch_size):
         print(i)
     
         img_path = source + 'image' + str(i) + '.png'
-        mask_path = source + 'image' + str(i) + '_poly.png'   
+        #mask_path = source + 'image' + str(i) + '_poly.png'   
         binmask_path = source + 'image' + str(i) + '_maskbin.png'
 
-        img = pad_zeros(io.imread(img_path)[:,:,:3],patch_size,patch_size,3)
-        mask = pad_zeros(io.imread(mask_path)[:,:,:3],patch_size,patch_size,3)
+        if normalize:
+            img = pad_zeros(batchStainNormalize(io.imread(img_path)[:,:,:3]),patch_size,patch_size,3)
+        else:    
+            img = pad_zeros(io.imread(img_path)[:,:,:3],patch_size,patch_size,3)
+        #mask = pad_zeros(io.imread(mask_path)[:,:,:3],patch_size,patch_size,3)
         binmask = pad_zeros(io.imread(binmask_path),patch_size,patch_size,1)
 
         img_patches = extract_patches(img,(patch_size,patch_size))
-        mask_patches = extract_patches(mask,(patch_size,patch_size))
+        #mask_patches = extract_patches(mask,(patch_size,patch_size))
         binmask_patches = extract_patches(binmask,(patch_size,patch_size))
 
-        for j,(img, mask, binmask) in enumerate(zip(img_patches, mask_patches, binmask_patches)):
+        for j,(img, binmask) in enumerate(zip(img_patches, binmask_patches)):
             io.imsave(PATH_IMG+'/'+ str(i)+'_'+str(j)+'.png', img)
-            io.imsave(PATH_MASK+'/'+ str(i)+'_'+str(j)+'.png', mask)
+            #io.imsave(PATH_MASK+'/'+ str(i)+'_'+str(j)+'.png', mask)
             io.imsave(PATH_BINMASK+'/'+ str(i)+'_'+str(j)+'.png', binmask)
 
-def createsBinMask(source):
+def createBinMask(source):
     """ Takes .txt file from source folder and
         creates a binary mask and save it there
     """
@@ -70,3 +75,10 @@ def createsBinMask(source):
             content = [bool(int(x.strip().split()[0]))*255 for x in content]
             content = np.array(content).reshape(shape)
             io.imsave(filename[:-4] + 'bin.png',content)
+
+if __name__ == "__main__":
+    source = '../segmentation_training_set/'
+    shape = 256
+    
+    createBinMask(source)
+    createImgPatches(source, shape)
