c51e8eded0ba05e091fd2430e8c407321db8444c
sanjaykr5 <sanjaysnj71@gmail.com>
2018-07-16 03:28:18 +0530

    Some Changes

--- DataLoader.py
+++ DataLoader.py
@@ -4,49 +4,34 @@ import numpy as np
 
 from torch.utils.data import Dataset
 from torchvision import transforms, utils
-
-from skimage import transform, io, img_as_float32
+from PIL import Image
 
 class NucleiSegTrain(Dataset):
 	""" Data Loader for Training data"""
-	def __init__(self, path='./Data/Train/', transforms=None, NCHW=True):
+	def __init__(self, path='./Data/Train/', transforms=None):
 		super(NucleiSegTrain, self).__init__()
 		self.path = path
 		if self.path[-1] != '/':
 			self.path = self.path + '/'
 		self.transforms = transforms
 		self.list = os.listdir(self.path + 'Images')
-		self.NCHW = NCHW        
 
 	def __len__(self):
 		return len(self.list)
 
-	def __getitem__(self,idx):
-		if self.path[-1] != '/':
-			self.path = self.path + '/'
-
-		IMG_PATH = self.path + 'Images/'
-		MASK_PATH = self.path + 'Masks/'
-
-		img = io.imread(IMG_PATH + self.list[idx])
-		img = img_as_float32(img)
-        
-		# Converting img from H*W*Ch to Ch*H*W
-		if self.NCHW == True:
-			img = np.moveaxis(img,2,0)
-                
-		mask = io.imread(MASK_PATH + self.list[idx])
-		mask = img_as_float32(mask)
-        
-		# Reshaping mask to correct format
-		if len(mask.shape) == 2:
-			mask = mask.reshape(1,mask.shape[0],mask.shape[1])
-
-
-		if self.transforms is not None:
-			img = self.transforms(img)
-			mask = self.transforms(mask)
-		return img, mask
+	def __getitem__(self, index):
+        image_path = 'Train/Image/'
+        mask_path = 'Train/Mask/'
+        image = Image.open(image_path + self.list[index])
+        image = image.convert('RGB')
+        mask = Image.open(mask_path + self.list[index])
+        mask = mask.convert('L')
+        if self.transforms is not None:
+            image = self.transforms(image)
+            mask = self.transforms(mask)
+            # If the transform variable is not empty
+            # then it applies the operations in the transforms with the order that it is created.
+        return image, mask
 
 class NucleiSegVal(Dataset):
 	""" Data Loader for Vaidation/Test data"""
@@ -62,31 +47,17 @@ class NucleiSegVal(Dataset):
 	def __len__(self):
 		return len(self.list)
 
-	def __getitem__(self,idx):
-		if self.path[-1] != '/':
-			self.path = self.path + '/'
-
-		IMG_PATH = self.path + 'Images/'
-		MASK_PATH = self.path + 'Masks/'
-
-		img = io.imread(IMG_PATH + self.list[idx])
-		img = img_as_float32(img)
-        
-		# Converting img from H*W*Ch to Ch*H*W
-		print('NCHW =',self.NCHW)        
-		if self.NCHW == True:
-			img = np.moveaxis(img,2,0)
-        
-		mask = io.imread(MASK_PATH + self.list[idx])
-		mask = img_as_float32(mask)
-
-		# Reshaping mask to correct format
-		print('mask shape = ',mask.shape)
-		if len(mask.shape) == 2:
-			mask = mask.reshape(1,mask.shape[0],mask.shape[1])        
-		print('mask shape = ',mask.shape)
-		if self.transforms is not None:
-			img = self.transforms(img)
-			mask = self.transforms(mask)
-		return img, mask, self.list[idx]
+	def __getitem__(self, index):
+        image_path = 'Train/Image/'
+        mask_path = 'Train/Mask/'
+        image = Image.open(image_path + self.list[index])
+        image = image.convert('RGB')
+        mask = Image.open(mask_path + self.list[index])
+        mask = mask.convert('L')
+        if self.transforms is not None:
+            image = self.transforms(image)
+            mask = self.transforms(mask)
+            # If the transform variable is not empty
+            # then it applies the operations in the transforms with the order that it is created.
+        return image, mask
 
--- train.py
+++ train.py
@@ -10,32 +10,26 @@ import os
 import numpy as np
 from Unet import Unet
 from DataLoader import NucleiSegTrain, NucleiSegVal
-from skimage import transform, io
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-import GPUtil
 
 
 
+class Average(object):
+    def __init__(self):
+        self.reset()
 
-class History(object):
-    """contains History of model like train losses and val losses"""
-    def __init__(self,train_loss=None,val_loss=None):
-        super(History, self).__init__()
-        self.train_loss = train_loss
-        self.val_loss = val_loss
-    
-    def get_train_loss(self):
-        return self.train_loss
-    
-    def get_val_loss(self):
-        return self.val_loss
-    
-    def plot_loss(self):
-        plt1, = plt.plot(self.train_loss, 'b-o', label="train loss")
-        plt2, = plt.plot(self.val_loss, 'g-o', label="test loss")
-        plt.legend([plt1, plt2], ['Train Loss', 'Val Loss'])
-        plt.show()
+    def reset(self):
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.sum += val
+        self.count += n
+
+    @property
+    def avg(self):
+        return self.sum / self.count
+
+
 
 def dice_loss(input, target):
 	input = torch.sigmoid(input)
@@ -48,17 +42,6 @@ def dice_loss(input, target):
 	return 1 - ((2. * intersection + smooth) /
 		(iflat.sum() + tflat.sum() + smooth))
 
-def batch(iterable1,batch_size):
-    """Yields lists by batch"""
-    b = []
-    for i, t in enumerate(iterable1):
-        b.append(t)
-        if (i + 1) % batch_size == 0:
-            yield np.array(b)
-            b = []
-
-    if len(b) > 0:
-        yield np.array(b)
 
 def train_net(net,train_set,test_set,epochs=5,batch_size=1,lr=0.1,val_ratio=0.1,save_model=True):
     cuda = torch.cuda.is_available()
@@ -70,10 +53,8 @@ def train_net(net,train_set,test_set,epochs=5,batch_size=1,lr=0.1,val_ratio=0.1,
     if not os.path.isdir('./Weights'):
         os.mkdir('./Weights')
         
-    N_train = len(train_set)
-    N_test = len(test_set)
     
-    optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=0.0005)
+    optimizer = torch.optim.Adam(net.parameters(), lr=1e-4)
     
     criterion = dice_loss
     print('''
@@ -81,10 +62,7 @@ def train_net(net,train_set,test_set,epochs=5,batch_size=1,lr=0.1,val_ratio=0.1,
         Epochs: {}
         Batch size: {}
         Learning rate: {}
-        Training size: {}
-        Validation size: {}
-    '''.format(epochs, batch_size, lr, N_train,
-               N_test))
+    '''.format(epochs, batch_size, lr)
     
     print('preparing training data .....')
     train_loader = DataLoader(train_set, batch_size = batch_size, shuffle = True)
@@ -94,68 +72,40 @@ def train_net(net,train_set,test_set,epochs=5,batch_size=1,lr=0.1,val_ratio=0.1,
     test_loader = DataLoader(test_set, batch_size = batch_size, shuffle = False)
     print('Done .....')
     
-    train_loss = []
-    test_loss = []
     for epoch in range(0,epochs):
         print('Starting epoch {}/{}.'.format(epoch + 1, epochs))
         
         # Setting network to training mode
         net.train()
-        
-        epoch_loss = 0
-        
+        train_loss = Average()
         for i,(imgs,t_masks) in enumerate(train_loader):
             if cuda:
                 imgs = imgs.cuda()
                 t_masks = t_masks.cuda()
-            if i==0:
-                print(imgs.shape)
-                print(t_masks.shape)
             imgs = Variable(imgs)
             t_masks = Variable(t_masks)
-
             p_masks = net(imgs)
-            
             loss = criterion(p_masks,t_masks)
-            epoch_loss += loss
-            
-            #print('{0:.4f} --- loss: {1:.6f}'.format(i * batch_size / N_train , loss ))
-            
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            train_loss.update(loss.item(), images.size(0))
         
-        train_loss.append(epoch_loss.cpu().detach().numpy())
-        print('Epoch finished ! Loss: {}'.format((epoch_loss*batch_size)/N_train))
-        
-        print(GPUtil.showUtilization())
         # setting network to evaluation mode
         net.eval()
-        
-        val_loss = 0
+        val_loss = Average()
         for i,(imgs,t_masks) in enumerate(test_loader):
-            if i==0:
-                print(imgs.dtype,imgs.shape)
-                print(t_masks.dtype,t_masks.shape)
             if cuda:
                 imgs = imgs.cuda()
                 t_masks = t_masks.cuda()
-                
             imgs = Variable(imgs)
             t_masks = Variable(t_masks)
-            
             p_masks = net(imgs)
-            
-            loss = criterion(p_masks,t_masks).cpu()
-            val_loss += loss.detach().numpy()
-            
-        test_loss.append(val_loss)    
-        print('Validation Dice Loss: {}'.format((val_loss*batch_size)/N_test))
-        
+            loss = criterion(p_masks,t_masks)
+            val_loss.update(vloss.item(), images.size(0))
+        print("Epoch {}, Loss: {}, Validation Loss: {}".format(epoch + 1, train_loss.avg, val_loss.avg))
         if save_model and epoch > epochs//2:
             torch.save(net.state_dict(), './Weights/cp_{}_{}.pth.tar'.format(epoch + 1, (val_loss*batch_size)/N_test))
-    Hist = History(train_loss, test_loss)
-    Hist.plot_loss()
     
 if __name__ == '__main__':
 
@@ -166,8 +116,10 @@ if __name__ == '__main__':
     lr = 1e-4
 
     # create datasets 
-    train_set = NucleiSegTrain(path = TRAIN_PATH, transforms=None)
-    test_set = NucleiSegVal(path = TEST_PATH, transforms=None)
+    transformations_train = transforms.Compose([transforms.ToTensor()])
+    transformations_val = transforms.Compose([transforms.ToTensor()])
+    train_set = NucleiSegTrain(path = TRAIN_PATH, transforms=transformations_train)
+    test_set = NucleiSegVal(path = TEST_PATH, transforms=transformations_val)
 
     net = Unet(3,1)
 
